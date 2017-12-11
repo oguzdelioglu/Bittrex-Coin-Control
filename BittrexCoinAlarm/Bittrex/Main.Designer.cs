be88d814c5ef6242fc5b895995c52612aa0cf490
oguzdelioglu <kemerkaya@hotmail.com.tr>
2017-12-11 23:21:09 +0300

    BittrexCoinAlarm
    BittrexCoinAlarm

---
+++ BittrexCoinAlarm/Bittrex/Main.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Bittrex
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.button1 = new System.Windows.Forms.Button();
+            this.coin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lowlimit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toplimit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.coinname = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.susturToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Control";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // coin
+            // 
+            this.coin.Location = new System.Drawing.Point(254, 3);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(100, 20);
+            this.coin.TabIndex = 1;
+            this.coin.Text = "STEEM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lower Limit:";
+            // 
+            // lowlimit
+            // 
+            this.lowlimit.Location = new System.Drawing.Point(254, 27);
+            this.lowlimit.Name = "lowlimit";
+            this.lowlimit.Size = new System.Drawing.Size(100, 20);
+            this.lowlimit.TabIndex = 3;
+            this.lowlimit.Text = "0,00011014";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Upper limit:";
+            // 
+            // toplimit
+            // 
+            this.toplimit.Location = new System.Drawing.Point(254, 53);
+            this.toplimit.Name = "toplimit";
+            this.toplimit.Size = new System.Drawing.Size(100, 20);
+            this.toplimit.TabIndex = 5;
+            this.toplimit.Text = "0,00011014";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(384, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = " Coin:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(424, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Last Value";
+            // 
+            // coinname
+            // 
+            this.coinname.AutoSize = true;
+            this.coinname.Location = new System.Drawing.Point(424, 30);
+            this.coinname.Name = "coinname";
+            this.coinname.Size = new System.Drawing.Size(0, 13);
+            this.coinname.TabIndex = 10;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(424, 53);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(0, 13);
+            this.lblPrice.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(384, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Price:";
+            // 
+            // time
+            // 
+            this.time.Interval = 1000;
+            this.time.Tick += new System.EventHandler(this.time_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem1,
+            this.susturToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 70);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(156, 22);
+            this.toolStripMenuItem2.Text = "Programa Geç";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(156, 22);
+            this.toolStripMenuItem1.Text = "Programı Kapat";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // susturToolStripMenuItem
+            // 
+            this.susturToolStripMenuItem.Name = "susturToolStripMenuItem";
+            this.susturToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.susturToolStripMenuItem.Text = "Sustur";
+            this.susturToolStripMenuItem.Click += new System.EventHandler(this.stopalarmturToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Bittrex";
+            this.notifyIcon1.Visible = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 30);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Mute Alarm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(495, 53);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Save Settings";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 137);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.coinname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.toplimit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lowlimit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coin);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ODEL Coin Alarm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox coin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lowlimit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox toplimit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label coinname;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer time;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem susturToolStripMenuItem;
+    }
+}
+
---
+++ BittrexCoinAlarm/Bittrex/Main.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Media;
+
+namespace Bittrex
+{
+
+    public partial class main : Form
+    {
+        MyProg.INIFile inif = new MyProg.INIFile(AppDomain.CurrentDomain.BaseDirectory + "\\config.ini");
+        public string coinnameString = "";
+        public string currency = "https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-" + "{0}";
+        public string data = "";
+        public string info1 = "Coin Lower Limit!";
+        public string info2 = "Coin Upper Limit!";
+        double price = 0;
+        public main()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("config.ini"))
+            {
+                    try
+                    {
+                    coin.Text = inif.Read("Settings", "coinname");
+                    toplimit.Text = inif.Read("Settings", "toplimit");
+                    lowlimit.Text = inif.Read("Settings", "lowlimit");
+                    }
+                    catch
+                    {
+
+                    }
+            }
+        }
+        public void getinfo()
+        {
+            JObject o = JObject.Parse(data);
+            string getinfo = "";
+            foreach (var x in o)
+            {
+                if (x.Key.ToString() == "result")
+                {
+                    getinfo = x.Value.Last["Last"].ToString();
+                    price = Convert.ToDouble(getinfo);
+                    coinname.Text = coinnameString;
+                    lblPrice.Text = price.ToString();
+                    this.Text = coinnameString + " | " + price.ToString();
+                }
+            }
+        }
+        public void control()
+        {
+            double alt = Convert.ToDouble(lowlimit.Text);
+            double ust = Convert.ToDouble(toplimit.Text);
+            if (price >= ust)
+            {
+                alarm(info2);
+            }
+            else if (price < alt)
+            {
+                alarm(info1);
+            }
+            else
+            {
+                time.Start();
+            }
+        }
+        public void alarm(String bilgitur)
+        {
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = coinnameString;
+            notifyIcon1.BalloonTipText = bilgitur + Environment.NewLine+ "Now:" + lblPrice.Text;
+            notifyIcon1.ShowBalloonTip(20000);
+            SoundPlayer player = new SoundPlayer();
+            string path = "C:\\windows\\media\\notify.wav"; // Müzik adresi
+            player.SoundLocation = path;
+            player.PlayLooping();
+        }
+        public void getdata()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(currency);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            data = reader.ReadToEnd();
+        }
+        public class Item
+        {
+            public string asks;
+        }
+        public void setprice(String coin)
+        {
+            coinnameString = coin;
+            currency = String.Format(currency, coin);
+        }
+        private void time_Tick(object sender, EventArgs e)
+        {
+            time.Stop();
+            try
+            {
+                setprice(coin.Text);
+                getdata();
+                getinfo();
+                control();
+            }
+            catch
+            {
+                time.Start();
+            } 
+        }
+        public void stopalarm()
+        {
+            SoundPlayer player = new SoundPlayer();
+            string path = "C:\\windows\\media\\notify.wav"; // Müzik adresi
+            player.SoundLocation = path;
+            player.Stop();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stopalarm();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            inif.Write("Settings", "coinname", coin.Text);
+            inif.Write("Settings", "lowlimit", lowlimit.Text);
+            inif.Write("Settings", "toplimit", toplimit.Text);
+        }
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void stopalarmturToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            stopalarm();
+        }
+    }
+  
+}
